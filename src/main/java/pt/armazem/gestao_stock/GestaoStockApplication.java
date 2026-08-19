6ec6b22d1240a1373806e03fa4ba297293260990
João Valério <joaosavalerio@gmail.com>
2026-08-19 18:56:42 +0100

    first commit

---
+++ src/main/java/pt/armazem/gestao_stock/GestaoStockApplication.java
@@ -0,0 +1,13 @@
+package pt.armazem.gestao_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/armazem/gestao_stock/GestaoStockApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.armazem.gestao_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
