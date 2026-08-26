bc5c2e602d61d1a9b6d8acd51882a914433df98a
João Valério <joaosavalerio@gmail.com>
2026-08-26 23:04:03 +0100

    finished entities

---
+++ src/main/java/pt/armazem/gestao_stock/domain/entities/Request.java
@@ -0,0 +1,5 @@
+package pt.armazem.gestao_stock.domain.entities;
+
+public class Request {
+
+}
---
+++ src/main/java/pt/armazem/gestao_stock/domain/entities/RequestItem.java
@@ -0,0 +1,8 @@
+package pt.armazem.gestao_stock.domain.entities;
+
+/**
+ * RequestItem
+ */
+public class RequestItem {
+
+}
