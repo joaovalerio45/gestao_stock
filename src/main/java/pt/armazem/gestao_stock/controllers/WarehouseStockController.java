3610a0da78429f3e262a8e0644257faf75726c48
João Valério <joaosavalerio@gmail.com>
2026-09-06 21:04:59 +0100

    added warehouse stock controller

--- src/main/java/pt/armazem/gestao_stock/controllers/WarehouseStockController.java
+++ src/main/java/pt/armazem/gestao_stock/controllers/WarehouseStockController.java
@@ -1,9 +1,12 @@
 package pt.armazem.gestao_stock.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import pt.armazem.gestao_stock.domain.entities.WarehouseStock;
 import pt.armazem.gestao_stock.services.WarehouseStockService;
 
 @RestController
@@ -12,6 +15,11 @@ import pt.armazem.gestao_stock.services.WarehouseStockService;
 public class WarehouseStockController {
 
     private final WarehouseStockService warehouseStockService;
+
+    @GetMapping("/{warehouseId}/items/{itemId}")
+    public WarehouseStock fetchWarehouseStockbyId(@PathVariable Long warehouseId, @PathVariable Long itemId){
+        return warehouseStockService.getWarehouseStock(warehouseId, itemId);
+    }
 
 }
 
